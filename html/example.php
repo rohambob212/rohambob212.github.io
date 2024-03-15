3f1a685ee323e8474935188f0476cd83dbc836ec
rohambob212 <rohambob2244@gmail.com>
2024-03-15 21:18:08 +0330

    changed website completely

--- html/example.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "Today's date is ".date('Y-m-d H:i:s');
--- html/wp-content/plugins/gpt3-ai-content-generator/admin/views/finetune/hyperparams.php
+++
@@ -1,4 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit;
-?>
-<p><strong><?php echo esc_html__('Epochs','gpt3-ai-content-generator')?>: </strong><?php echo esc_html($wpaicg_data->n_epochs);?></p>
