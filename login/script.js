7fb2cdff3edc95a9a65b1a59f1ce846637356b3d
rohambob212 <rohambob2244@gmail.com>
2025-02-09 17:24:21 +0330

    added the flicking light

--- login/script.js
+++ login/script.js
@@ -3,7 +3,7 @@ function sleep(ms) {
 }
 let takinginput = false
 document.addEventListener("DOMContentLoaded", async function() {
-
+    document.getElementById("waiter").style.backgroundColor = "Black";
     lines = [document.getElementById("firstline"),document.getElementById("secondline"),document.getElementById("thirdline"),document.getElementById("fourthline"),document.getElementById("fifthline")];
     linetexts = ["starting...",">logging in...",">ERORR",">user not authenticated",">Please enter Your Password To Authenticate "];
     for (let i = 0; i < lines.length; i++) {
@@ -13,8 +13,23 @@ document.addEventListener("DOMContentLoaded", async function() {
             await sleep(125)
             line.innerHTML += clinetext[i]
         }
-        await sleep(750)
+        await sleep(450)
+    }
+    takinginput = true
+    document.getElementById("waiter").style.backgroundColor = "White"
+    let waiter = document.getElementById("waiter")
+    while (takinginput) {
+        flick(waiter)
+        await sleep(500)
     }
 
 });
 
+function flick(ttf) {
+    if (ttf.style.backgroundColor === "white") {
+        ttf.style.backgroundColor = "Black"
+    }
+    else if (ttf.style.backgroundColor === "black") {
+        ttf.style.backgroundColor = "White"
+    }
+}
